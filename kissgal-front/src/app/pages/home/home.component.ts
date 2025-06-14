881208eb8a61ef07d952f6ed583ebbb72e9420f2
lapsus63 <lapsus63@gmail.com>
2025-06-14 11:28:10 +0200

    Theming colors and menu

--- kissgal-front/src/app/pages/home/home.component.ts
+++ kissgal-front/src/app/pages/home/home.component.ts
@@ -1,6 +1,5 @@
-import { Component, OnDestroy } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { BehaviorSubject, timer } from 'rxjs';
+import {Component} from '@angular/core';
+import {CommonModule} from '@angular/common';
 
 @Component({
   selector: 'app-home',
@@ -8,23 +7,6 @@ import { BehaviorSubject, timer } from 'rxjs';
   styleUrls: ['./home.component.scss'],
   imports: [CommonModule]
 })
-export class HomeComponent implements OnDestroy {
-  private copyMessageSubject = new BehaviorSubject<string | null>(null);
-  copyMessage$ = this.copyMessageSubject.asObservable(); // Expose as Observable
+export class HomeComponent {
 
-  copyToClipboard(text: string): void {
-    navigator.clipboard
-      .writeText(text)
-      .then(() => {
-        this.copyMessageSubject.next(`"${text}" copied to clipboard!`);
-        timer(3000).subscribe(() => this.copyMessageSubject.next(null));
-      })
-      .catch((err) => {
-        this.copyMessageSubject.next('Failed to copy text. Please try again.');
-      });
-  }
-
-  ngOnDestroy(): void {
-    this.copyMessageSubject.complete();
-  }
 }
