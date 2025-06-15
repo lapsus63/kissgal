8d0d95a7380ce46675e6aa9fd41a42e778973610
lapsus63 <lapsus63@gmail.com>
2025-06-15 10:49:12 +0200

    Homepage layout

--- kissgal-front/src/app/pages/home/home.component.ts
+++ kissgal-front/src/app/pages/home/home.component.ts
@@ -1,11 +1,13 @@
 import {Component} from '@angular/core';
 import {CommonModule} from '@angular/common';
+import {MatDivider} from "@angular/material/divider";
+import {TranslateModule} from "@ngx-translate/core";
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
-  imports: [CommonModule]
+  imports: [CommonModule, MatDivider, TranslateModule]
 })
 export class HomeComponent {
 
